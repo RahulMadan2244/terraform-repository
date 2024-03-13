1be505ef8b121e71e46a2006b0a02f0903c59301
Rahul madan <120299635+RahulMadan2244@users.noreply.github.com>
2024-03-13 15:18:51 +0530

    Update gcs-bucket-for-tf-state.tf
    Final file

--- gcs-bucket-for-tf-state.tf
+++ gcs-bucket-for-tf-state.tf
@@ -1,15 +1,15 @@
 #Terraform state in a Cloud Storage bucket
-backend "gcs" {
+/*backend "gcs" {
    bucket  = "rahul-gcs-bucket-for-tf-state-file"
    prefix  = "terraform/state"
 }
-
+*/
 resource "google_storage_bucket" "rahul-gcs-bucket-for-tf-state-file" {
         name = "rahul-gcs-bucket-for-tf-state-file"
         location = "US"
         force_destroy = "true"
         retention_policy {
-          retention_period = "2,147,483"
+          retention_period = "3000"
     }
         versioning {
           enabled = true
