6390f711b32c6725001142c6a82da2e8fd51550f
Rahul madan <120299635+RahulMadan2244@users.noreply.github.com>
2024-03-13 15:38:41 +0530

    Update gcs-bucket-for-tf-state.tf
    test

--- gcs-bucket-for-tf-state.tf
+++ gcs-bucket-for-tf-state.tf
@@ -8,9 +8,6 @@ resource "google_storage_bucket" "rahul-gcs-bucket-for-tf-state-file" {
         name = "rahul-gcs-bucket-for-tf-state-file"
         location = "US"
         force_destroy = "true"
-        retention_policy {
-          retention_period = "3000"
-    }
         versioning {
           enabled = true
    }
